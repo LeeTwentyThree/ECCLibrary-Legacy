41eb48167d116e2e2b12cdbe77d059bfbef344b9
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2020-12-20 01:21:28 -0500

    Initial commit

---
+++ ECC Library/ECC Library/ModAudio.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ECCLibrary
+{
+    public class ModAudio
+    {
+        private AudioClip[] allClips;
+
+        public void Init(AssetBundle assetBundle)
+        {
+            allClips = assetBundle.LoadAllAssets<AudioClip>();
+        }
+
+        public AudioClipPool CreateClipPool(string startingLetters)
+        {
+            List<AudioClip> clips = new List<AudioClip>();
+            for (int i = 0; i < allClips.Length; i++)
+            {
+                if (allClips[i].name.StartsWith(startingLetters))
+                {
+                    clips.Add(allClips[i]);
+                }
+            }
+            return new AudioClipPool(clips.ToArray());
+        }
+
+        public class AudioClipPool
+        {
+            public AudioClip[] clips;
+
+            public AudioClipPool(AudioClip[] clips)
+            {
+                this.clips = clips;
+            }
+
+            public AudioClip GetRandomClip()
+            {
+                return clips[UnityEngine.Random.Range(0, clips.Length)];
+            }
+        }
+    }
+}
---
+++ ECC Library/ECC Library/QPatch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QModManager.API.ModLoading;
+using QModManager.Utility;
+using UnityEngine;
+
+namespace ECC_Library
+{
+    [QModCore]
+    public class QPatch
+    {
+        [QModPatch]
+        public static void Patch()
+        {
+            Debug.Log("Eel's Creature Creator loaded.");
+        }
+    }
+}
